17de9e785586897f6caa84626d47e48835100317
RSushil1 <sushilsinghrathore1998@gmail.com>
2023-06-14 19:09:01 +0530

    all crud added

--- backend/app.js
+++ backend/app.js
@@ -18,12 +18,12 @@ app.get('/', (req, res) => {
     res.json("Hello Api");
 })
 
-app.get('/students', (req, res)=>{
-    res.json(students);
-})
-app.get('/students/:id', (req, res)=>{
+// app.get('/students', (req, res) => {
+//     res.json(students);
+// })
+app.get('/students/:id', (req, res) => {
     let id = req.params.id
-    let std = students.find(x=>x.id==id)
+    let std = students.find(x => x.id == id)
     res.json(std);
 })
 app.get('/students/name/:name', (req, res) => {
@@ -33,27 +33,27 @@ app.get('/students/name/:name', (req, res) => {
 });
 
 // Post Method
-app.post('/students', (req, res)=>{
+app.post('/students', (req, res) => {
     // let first_name = req.body.first_name
     // let last_name = req.body.last_name
     // let email = req.body.email
 
 
-    if(!req.body.first_name){
+    if (!req.body.first_name) {
         res.status(400);
-        return res.json({error: "first name is required..."})
+        return res.json({ error: "first name is required..." })
     }
-    if(!req.body.last_name){
+    if (!req.body.last_name) {
         res.status(400);
-       return res.json({error: "last name is required..."})
+        return res.json({ error: "last name is required..." })
     }
-    if(!req.body.email){
+    if (!req.body.email) {
         res.status(400);
-        return res.json({error: "email is required..."})
+        return res.json({ error: "email is required..." })
     }
 
     const user = {
-        id: students.length +1,
+        id: students.length + 1,
         first_name: req.body.first_name,
         last_name: req.body.last_name,
         email: req.body.email
@@ -64,24 +64,40 @@ app.post('/students', (req, res)=>{
 })
 
 // Put Method
-app.put('/students/:id', (req, res)=>{
+app.put('/students/:id', (req, res) => {
     let id = req.params.id
     let first_name = req.body.first_name
     let last_name = req.body.last_name
     let email = req.body.email
+    let age = req.body.age
+    let phone_number = req.body.phone_number
+    let address = req.body.address
+    let city = req.body.city
+    let major = req.body.major
+    let gpa = req.body.gpa
+    let graduation_date = req.body.graduation_date
+    let gender = req.body.gender
 
-    let index = students.findIndex((student)=>{
+    let index = students.findIndex((student) => {
         return (student.id == Number.parseInt(id))
     })
 
-    if (index >=0){
+    if (index >= 0) {
         let std = students[index]
         std.first_name = first_name
         std.last_name = last_name
         std.email = email
+        std.age = req.body.age
+        std.phone_number = req.body.phone_number
+        std.address = req.body.address
+        std.city = req.body.city
+        std.major = req.body.major
+        std.gpa = req.body.gpa
+        std.graduation_date = req.body.graduation_date
+        std.gender = req.body.gender
         res.json(std)
     }
-    else{
+    else {
         res.status(400)
         res.end()
     }
@@ -89,20 +105,34 @@ app.put('/students/:id', (req, res)=>{
 })
 
 // Delete Method
-app.delete('/students/:id', (req, res)=>{
-    let id = req.params.id;
-    let index=  students.findIndex((student)=>{
+app.delete('/students/:student_id', (req, res) => {
+    let id = req.params.student_id;
+    let index = students.findIndex((student) => {
         return (student.id == Number.parseInt(id))
     })
 
-    if(index >=0){
+    if (index >= 0) {
         let std = students[index]
         students.splice(index, 1)
         res.json(std)
     }
-    else{
+    else {
         res.status(400)
         res.end()
     }
 
 })
+
+//retrieving paginated data
+app.get('/students', (req, res) => {
+    let page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+
+
+    let startIndex = (page - 1) * limit;
+    let endIndex = (page) * limit;
+    let results = students.slice(startIndex, endIndex);
+
+    res.json(results);
+
+});
--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,11 @@
-// Get list
-fetch("http://localhost:3000/students").then((data) => {
-    // console.log(data);
+// Get list by pagination
+const defaultPage = 1;
+const defaultLimit = 10;
+fetchPaginatedData(defaultPage, defaultLimit);
+
+function fetchPaginatedData(page, limit) {
+  fetch(`http://localhost:3000/students?page=${page}&limit=${limit}`).then((data) => {
+    
     return data.json();
 }).then((objectData) => {
     // console.log(objectData);
@@ -12,50 +17,7 @@ fetch("http://localhost:3000/students").then((data) => {
                   <td>${values.last_name}</td>
                   <td>${values.email}</td>
                   <td>
-                  <!-- Button trigger modal -->
-<button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-  EDIT
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-
-      <form id="editForm${values.id}" action="">
-      <div class="form-floating mb-3">
-          <input type="first name" name="first_name" class="form-control" id="first_name"
-              placeholder="name@example.com">
-          <label for="floatingInput">First Name</label>
-      </div>
-      <div class="form-floating mb-3">
-          <input type="last name" name="last_name" class="form-control" id="last_name"
-              placeholder="name@example.com">
-          <label for="floatingInput">Last Name</label>
-      </div>
-      <div class="form-floating mb-3">
-          <input type="text" name="email" class="form-control" id="email"
-              placeholder="name@example.com">
-          <label for="floatingInput">Email address</label>
-      </div>
-
-</div>
-<div class="modal-footer">
-  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-  <button type="submit" value="Refresh Page" onClick = "handleEditSubmit(${values.id})"
-      class="btn btn-primary">Save</button>
-</div>
-</form>
-      </div> 
-    </div>
-  </div>
-</div>
-                  <button id = "${values.id}" onClick = "handleDSubmit(event)" type="submit" class="btn btn-danger">DELETE</button>
+                  <button id = "${values.id}" onClick = "handleViewSubmit(event)" type="submit" class="btn btn-primary btn-sm">Details</button>
                   </td>
                 </tr>`;
     })
@@ -63,86 +25,20 @@ fetch("http://localhost:3000/students").then((data) => {
         innerHTML = tableData;
 
 })
+}
 
+function handleViewSubmit(event) {
+    let studentId = event.target.id;
+    localStorage.setItem("id",studentId);
+    window.location.href="./details.html";
+}
 
-// Get list by Search
-// let Sform = document.getElementById('Sform');
-// Sform.addEventListener('submit', handleSearchSubmit);
-
-// function handleSearchSubmit(event) {
-// // event.preventDefault();
-
-//   let searchInput = document.getElementById('search-input').value;
-//   console.log(searchInput);
-//   let apiUrl = `http://localhost:3000/students/name/${searchInput}`;
-
-
-//     fetch(apiUrl).then((data) => {
-//         //  console.log(data);
-//         return data.json();
-//     }).then((objectData1) => {
-//         // console.log(objectData);
-//         let tableData = "";
-//         objectData1.map((values) => {
-//             tableData += `<tr>
-//                       <td>${values.id}</td>
-//                       <td>${values.first_name}</td>
-//                       <td>${values.last_name}</td>
-//                       <td>${values.email}</td>
-//                       <td>
-//                       <!-- Button trigger modal -->
-//     <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-//       EDIT
-//     </button>
-
-//     <!-- Modal -->
-//     <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-//       <div class="modal-dialog">
-//         <div class="modal-content">
-//           <div class="modal-header">
-//             <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
-//             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-//           </div>
-//           <div class="modal-body">
-
-//           <form id="editForm${values.id}" action="">
-//           <div class="form-floating mb-3">
-//               <input type="first name" name="first_name" class="form-control" id="first_name"
-//                   placeholder="name@example.com">
-//               <label for="floatingInput">First Name</label>
-//           </div>
-//           <div class="form-floating mb-3">
-//               <input type="last name" name="last_name" class="form-control" id="last_name"
-//                   placeholder="name@example.com">
-//               <label for="floatingInput">Last Name</label>
-//           </div>
-//           <div class="form-floating mb-3">
-//               <input type="text" name="email" class="form-control" id="email"
-//                   placeholder="name@example.com">
-//               <label for="floatingInput">Email address</label>
-//           </div>
-
-//     </div>
-//     <div class="modal-footer">
-//       <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-//       <button type="submit" value="Refresh Page" onClick = "handleEditSubmit(${values.id})"
-//           class="btn btn-primary">Save</button>
-//     </div>
-//     </form>
-//           </div> 
-//         </div>
-//       </div>
-//     </div>
-//                       <button id = "${values.id}" onClick = "handleDSubmit(event)" type="submit" class="btn btn-danger">DELETE</button>
-//                       </td>
-//                     </tr>`;
-//         })
-//         document.getElementById("table_body1").
-//             innerHTML = tableData;
-
-//     })
-// //  }
-
+let Nsearch = document.getElementById("Nsearch");
+Nsearch.addEventListener("submit",search);
+function search(event){
+    let name = document.getElementById("name").value;
+    localStorage.setItem("name", name);
+}
 
 
 // post request
@@ -177,62 +73,6 @@ function refreshPage() {
     window.location.reload();
 }
 
-// PUT Request
-
-function handleEditSubmit(studentId) {
-    const editForm = document.getElementById(`editForm${studentId}`);
-    console.log(editForm)
-    const formData = new FormData(editForm);
-    const data = Object.fromEntries(formData);
-    const jsonData = JSON.stringify(data);
-
-    fetch(`http://localhost:3000/students/${studentId}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: jsonData,
-    })
-        .then((res) => {
-            if (!res.ok) {
-                console.log("Problem updating student");
-                return;
-            }
-            return res.json();
-        })
-        .then((result) => {
-            console.log(result);
-            // Refresh the page or update the list after successful update
-            refreshPage();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-}
-
-// Delete request
-function handleDSubmit(event) {
-    const studentId = event.target.id;
-
-    fetch(`http://localhost:3000/students/${studentId}`, {
-        method: 'DELETE',
-    })
-        .then((res) => {
-            if (!res.ok) {
-                console.log("Problem deleting student");
-                return;
-            }
-            return res.json();
-        })
-        .then((result) => {
-            console.log(result);
-            // Refresh the page or update the list after successful deletion
-            refreshPage();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-}
 
 // Search list
 const searchFun = () => {
